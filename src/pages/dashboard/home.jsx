38251f8e6cc3f5435f1eae7faee24199721f16d8
Marlon Lúcio <marlonlucioss@gmail.com>
2023-12-14 23:28:33 -0300

    adiconando calendario

--- src/pages/dashboard/home.jsx
+++ src/pages/dashboard/home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   Typography,
   Card,
@@ -26,8 +26,41 @@ import {
   ordersOverviewData,
 } from "@/data";
 import { CheckCircleIcon, ClockIcon } from "@heroicons/react/24/solid";
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+const events = [
+  { title: 'Meeting', start: new Date() }
+]
+const handleDateClick = (selected) => {
+  const title = prompt("Please enter a new title for your event");
+  const calendarApi = selected.view.calendar;
+  calendarApi.unselect();
 
+  if (title) {
+    calendarApi.addEvent({
+      id: `${selected.dateStr}-${title}`,
+      title,
+      start: selected.startStr,
+      end: selected.endStr,
+      allDay: selected.allDay,
+    });
+  }
+};
+
+const handleEventClick = (selected) => {
+  if (
+    window.confirm(
+      `Are you sure you want to delete the event '${selected.event.title}'`
+    )
+  ) {
+    selected.event.remove();
+  }
+};
 export function Home() {
+  const [currentEvents, setCurrentEvents] = useState([]);
   return (
     <div className="mt-12">
       <div className="mb-12 grid gap-y-10 gap-x-6 md:grid-cols-2 xl:grid-cols-4">
@@ -251,6 +284,40 @@ export function Home() {
           </CardBody>
         </Card>
       </div>
+      <FullCalendar
+        height="75vh"
+        plugins={[
+          dayGridPlugin,
+          timeGridPlugin,
+          interactionPlugin,
+          listPlugin,
+        ]}
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
+        }}
+        initialView="dayGridMonth"
+        editable={true}
+        selectable={true}
+        selectMirror={true}
+        dayMaxEvents={true}
+        select={handleDateClick}
+        eventClick={handleEventClick}
+        eventsSet={(events) => setCurrentEvents(events)}
+        initialEvents={[
+          {
+            id: "12315",
+            title: "All-day event",
+            date: "2022-09-14",
+          },
+          {
+            id: "5123",
+            title: "Timed event",
+            date: "2022-09-28",
+          },
+        ]}
+      />
     </div>
   );
 }
